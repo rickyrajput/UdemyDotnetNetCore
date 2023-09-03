ae1d798abfe35342a05e5e7e993be6c4e8bd83d3
LAPTOP-RICKY\vicky <brajput@vcidata.com>
2023-09-03 11:55:41 -0400

    End of section 4

---
+++ API/Program.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using API.Data;
+using API.Interface;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options=>
+        {
+            options.TokenValidationParameters  = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =  new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+var app = builder.Build();
+
+app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
