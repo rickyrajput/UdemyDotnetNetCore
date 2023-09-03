37690ab44589cd6ea98ad2dc311af8e26867fb80
LAPTOP-RICKY\vicky <brajput@vcidata.com>
2023-09-03 13:27:51 -0400

    committed

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
